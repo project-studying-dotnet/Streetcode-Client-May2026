5557a7468f1d056cae0c8c2ae8c6e5d702ef5b08
AnnSnitko <annsnitko.com.com@gmail.com>
2026-05-19 23:29:25 +0200

    fix

--- src/features/StreetcodePage/Streetcode.component.tsx
+++ src/features/StreetcodePage/Streetcode.component.tsx
@@ -60,16 +60,6 @@ const StreetcodeContent = () => {
     };
 
     useAsync(() => {
-        // Promise.all([checkStreetcodeExist(streetcodeUrl.current)])
-        //     .then((response) => {
-        //         if (!response[0]) {
-        //             navigate(`${FRONTEND_ROUTES.OTHER_PAGES.ERROR404}`, { replace: true });
-        //         }
-        //     })
-        //     .catch((error) => {
-        //         console.error('Error checking streetcode:', error);
-        //     });
-
         const idParam = searchParams.get('qrid');
         if (idParam !== null) {
             const tempId = +idParam;
