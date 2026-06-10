f9f099f1512575017a0c9e75407725b980468c48
andrxpie <andrxpie@gmail.com>
2026-06-10 14:27:14 +0300

    Aligns project styling and extends type definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,13 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.svg' {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-    export default content;
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
 }
-declare module '*.css';
-declare module '*.jpeg';
-declare module '*.scss' {
+declare module "*.css";
+declare module "*.scss" {
   const classes: { [key: string]: string };
   export default classes;
 }
